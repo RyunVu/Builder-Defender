fa488e63958b4061c5312dec8fa224e24243fbb2
RyunVu <ryun3980@gmail.com>
2023-03-26 21:11:18 +0700

    Building Demolish

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -6,6 +6,13 @@ public class Building : MonoBehaviour{
 
     private BuildingTypeSO buildingType;
     private HealthSystem healthSystem;
+    private Transform buildingDemolishBtn;
+
+    private void Awake() {
+        buildingDemolishBtn = transform.Find("pfBuidlingDemolishBtn");
+        HideBuildingDemolishBtn();
+    }
+
     private void Start() {
         buildingType = GetComponent<BuildingTypeHolder>().buildingType;
         healthSystem = GetComponent<HealthSystem>();
@@ -16,5 +23,25 @@ public class Building : MonoBehaviour{
     
     private void HealthSystem_OnDied(object sender, System.EventArgs e) {
         Destroy(gameObject);
+    }
+
+    private void OnMouseEnter() {
+        ShowBuildingDemolishBtn();
+    }
+
+    private void OnMouseExit() {
+        HideBuildingDemolishBtn();
+    }
+
+    private void ShowBuildingDemolishBtn() {
+        if (buildingDemolishBtn != null) {
+            buildingDemolishBtn.gameObject.SetActive(true);
+        }
+    }
+
+    private void HideBuildingDemolishBtn() {
+        if (buildingDemolishBtn != null) {
+            buildingDemolishBtn.gameObject.SetActive(false);
+        }
     }
 }
